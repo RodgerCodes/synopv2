c917326c4795b8635e8b300afb0b0582746c3f92
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2022-07-26 02:04:45 +0200

    Update add.dart

--- lib/presentation/screens/regular/Home/add.dart
+++ lib/presentation/screens/regular/Home/add.dart
@@ -5,6 +5,10 @@ class AddData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(),
+      ),
+    );
   }
 }
